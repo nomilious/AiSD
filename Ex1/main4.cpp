bfee87b1bd000d990233926ad8aad5d0075802f8
Edward Belic <goingdfreal@gmail.com>
2022-03-25 20:40:35 +0300

    I've done all variantes of ex1!

--- Ex1/main4.cpp
+++ Ex1/main4.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 #include <sstream>
-int countBit(int, int);
+int flipBit(unsigned int, unsigned int);
 template <typename Stream>
 int run(Stream&);
 int test();
 
 int main() {
-    // std::cout << run(std::cin);
-    test();
+    std::cout << run(std::cin);
+    // test();
 
     return 0;
 }
-int countBit(int n, int k) {
+int flipBit(unsigned int n, unsigned int k) {
     return (n ^ (1 << k));
 }
 int test() {
     std::stringstream s1, s2, s3;
 
-    s1 << "25 1" << std::endl;
-    s2 << "25 4" << std::endl;
-    s3 << "1 0" << std::endl;
+    s1 << 25 << std::endl
+       << 1 << std::endl;
+    s2 << 25 << std::endl
+       << 4 << std::endl;
+    s3 << 1 << std::endl
+       << 0 << std::endl;
     std::cout
         << (run(s1) == 27) << std::endl
         << (run(s2) == 9) << std::endl
@@ -27,10 +30,64 @@ int test() {
 
     return 0;
 }
+int test2() {
+    std::stringstream s1, s2, s3;
+    int i = 0;
+    s1 << 31 << std::endl
+       << i++ << std::endl;
+    std::cout << (run(s1) == 30);
+    s1.str("");
+    s1 << 31 << std::endl
+       << i++ << std::endl;  // 1
+    std::cout << (run(s1) == 29);
+    s1.str("");
+    s1 << 31 << std::endl
+       << i++ << std::endl;  // 2
+    std::cout << (run(s1) == 27);
+    s1.str("");
+    s1 << 31 << std::endl
+       << i++ << std::endl;  // 3
+    std::cout << (run(s1) == 23);
+    s1.str("");
+    s1 << 31 << std::endl
+       << i++ << std::endl;  // 4
+    std::cout << (run(s1) == 15);
+    s1.str("");
+    i = 0;
+    s1 << 32 << std::endl
+       << i++ << std::endl;  // 0
+    std::cout << (run(s1) == 33);
+    s1.str("");
+    s1 << 32 << std::endl
+       << i++ << std::endl;  // 1
+    std::cout << (run(s1) == 34);
+    s1.str("");
+    s1 << 32 << std::endl
+       << i++ << std::endl;  // 2
+    std::cout << (run(s1) == 36);
+    s1.str("");
+    s1 << 32 << std::endl
+       << i++ << std::endl;  // 3
+    std::cout << (run(s1) == 40);
+    s1.str("");
+    s1 << 32 << std::endl
+       << i++ << std::endl;  // 4
+    std::cout << (run(s1) == 48);
+    s1.str("");
+    s1 << 32 << std::endl
+       << i++ << std::endl;  // 5
+    std::cout << (run(s1) == 0);
+    s1.str("");
+    s1 << 32 << std::endl
+       << i++ << std::endl;  // 6
+    std::cout << (run(s1) == 96);
+    s1.str("");
+    return 0;
+}
 template <typename Stream>
 int run(Stream& stream) {
-    int N, k;
+    unsigned int N, k;
 
     stream >> N >> k;
-    return countBit(N, k);
+    return flipBit(N, k);
 }
